c5289368b3c429056dc60bb63b8d1e3071b888f5
Sarah Shimizu <SarahS@MacBook-Pro-66.local>
2019-02-11 14:11:51 -0700

    Updates

---
+++ precache-manifest.221de2a40165b1181bf9eb95aeeab291.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "81205f1b664e1499add7",
+    "url": "/React/static/js/main.81205f1b.chunk.js"
+  },
+  {
+    "revision": "981f7e179878d745b3b6",
+    "url": "/React/static/js/1.981f7e17.chunk.js"
+  },
+  {
+    "revision": "81205f1b664e1499add7",
+    "url": "/React/static/css/main.44ac7ef5.chunk.css"
+  },
+  {
+    "revision": "2e4aef5b690b35b0918ee900b29f12c1",
+    "url": "/React/index.html"
+  }
+];
