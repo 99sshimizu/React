6de83680103c588059e004384cd5ceac166d6698
99sshimizu <sshimizu@ualberta.ca>
2019-02-15 21:38:23 -0700

    Updates

--- precache-manifest.7033e9441bd768e496334a5c57feac04.js
+++ precache-manifest.7033e9441bd768e496334a5c57feac04.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React/static/js/runtime~main.bdeb66da.js"
   },
   {
-    "revision": "a7c5215d8767ed1eab93",
-    "url": "/React/static/js/main.a7c5215d.chunk.js"
+    "revision": "0eb7c0b5d4f082fa25fc",
+    "url": "/React/static/js/main.0eb7c0b5.chunk.js"
   },
   {
     "revision": "981f7e179878d745b3b6",
     "url": "/React/static/js/1.981f7e17.chunk.js"
   },
   {
-    "revision": "a7c5215d8767ed1eab93",
+    "revision": "0eb7c0b5d4f082fa25fc",
     "url": "/React/static/css/main.44ac7ef5.chunk.css"
   },
   {
-    "revision": "f12ad0da501799263d87319cbcd510dd",
+    "revision": "7a774372e9c17353ccaacc2588cfb015",
     "url": "/React/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React/precache-manifest.dcc00f881878b55cbd11ccd8366710a4.js"
+  "/React/precache-manifest.7033e9441bd768e496334a5c57feac04.js"
 );
 
 workbox.clientsClaim();
