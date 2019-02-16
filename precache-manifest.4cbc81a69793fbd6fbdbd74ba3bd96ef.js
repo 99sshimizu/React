e586ab6ad01bd265b35a2f60e486b937f715d727
99sshimizu <sshimizu@ualberta.ca>
2019-02-15 20:19:05 -0700

    update

--- precache-manifest.4cbc81a69793fbd6fbdbd74ba3bd96ef.js
+++ precache-manifest.4cbc81a69793fbd6fbdbd74ba3bd96ef.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React/static/js/runtime~main.bdeb66da.js"
   },
   {
-    "revision": "81205f1b664e1499add7",
-    "url": "/React/static/js/main.81205f1b.chunk.js"
+    "revision": "666df2d7110a20b471b2",
+    "url": "/React/static/js/main.666df2d7.chunk.js"
   },
   {
     "revision": "981f7e179878d745b3b6",
     "url": "/React/static/js/1.981f7e17.chunk.js"
   },
   {
-    "revision": "81205f1b664e1499add7",
+    "revision": "666df2d7110a20b471b2",
     "url": "/React/static/css/main.44ac7ef5.chunk.css"
   },
   {
-    "revision": "2e4aef5b690b35b0918ee900b29f12c1",
+    "revision": "c55c49bd168179d83fba82ab87e7d781",
     "url": "/React/index.html"
   }
 ];
